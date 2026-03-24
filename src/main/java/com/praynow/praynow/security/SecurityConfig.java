bc014d234e5904fa9fee6220bf95d956956d962b
Zaid Sikander <zaidsikander@Zaids-MacBook-Air.local>
2026-03-24 15:18:04 -0700

    Security change for mainpage

--- src/main/java/com/praynow/praynow/security/SecurityConfig.java
+++ src/main/java/com/praynow/praynow/security/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/", "/index.html").permitAll()
+                
                 .requestMatchers("/auth/login").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/masjids/**").permitAll()
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
