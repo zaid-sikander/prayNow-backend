503142e4011021683e05bdab4086962e60d55277
Zaid Sikander <zaidsikander@Zaids-MacBook-Air.local>
2026-03-24 14:43:34 -0700

    Added swagger details

--- src/main/java/com/praynow/praynow/controller/MasjidController.java
+++ src/main/java/com/praynow/praynow/controller/MasjidController.java
@@ -6,6 +6,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+
 @RestController
 @RequestMapping("/api/masjids")
 public class MasjidController {
@@ -15,21 +19,32 @@ public class MasjidController {
         this.masjidService = masjidService;
     }
 
+    @Operation(summary = "Add a Masjid Schedule")
+    @ApiResponse(responseCode = "200", description = "Schedule added success") 
+    @ApiResponse(responseCode = "403", description = "Missing or invalid JWT token")
     @PostMapping
     public ResponseEntity<Masjid> create(@RequestBody Masjid masjid) {
         return ResponseEntity.ok(masjidService.save(masjid));
     }
 
+    @Operation(summary = "Get all Masjid's schedules")
+    @ApiResponse(responseCode =  "200", description = "Success")
     @GetMapping
     public ResponseEntity<List<Masjid>> getAll() {
         return ResponseEntity.ok(masjidService.getAll());
     }
 
+    @Operation(summary = "Get all the schedules for a Masjid by Id")
+    @ApiResponse(responseCode =  "200", description = "Success")
+    @ApiResponse(responseCode =  "404", description = "Masjid not Found")
     @GetMapping("/{masjidId}")
     public ResponseEntity<List<Masjid>> getByMasjidId(@PathVariable String masjidId) {
         return ResponseEntity.ok(masjidService.getByMasjidId(masjidId));
     }
 
+    @Operation(summary = "Get the schedule for a Masjid by Id, year, and month")
+    @ApiResponse(responseCode =  "200", description = "Success")
+    @ApiResponse(responseCode =  "404", description = "Masjid, Id, or year not Found")
     @GetMapping("/{masjidId}/{year}/{month}")
     public ResponseEntity<Masjid> getSchedule(
             @PathVariable String masjidId,
